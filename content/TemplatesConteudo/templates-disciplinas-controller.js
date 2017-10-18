47c7d2e1f2b47b96e6788dfab0ad936702958e97
Natalia Marufuji Aoki <nataliamarufuji@gmail.com>
2017-10-18 19:20:45 -0200

    Add - Conteúdo das matérias de primeiro semestre

--- content/TemplatesConteudo/templates-disciplinas-controller.js
+++ content/TemplatesConteudo/templates-disciplinas-controller.js
@@ -17,19 +17,120 @@ angular.module('ocurso').controller("TemplatesDisciplinasController", function (
 	var arrayPrimeiroSemestre = [
 		{
 			titulo: 'ComputacaoSociedade',
-			conteudo: '<p>O Bacharelado em Engenharia de Software foi criado em 2009, sendo a UFG a universidade brasileira pioneira. O curso é focado na graduação de um profissional capaz de especificar software; projetar (design) software (inclui arquitetura de software); construir (programar) software com qualidade; realizar atividades de manutenção de software, além de trabalhar em equipe, comunicar e escrever bem. Tem a duração de 9 semestres, predominantesmente no turno noturno.</p>'	
+			conteudo: '<b>Características</b></br><br>\
+						- Núcleo Comum</br>\
+						- Disciplina ministrada pelo INF (Instituto de Informática)</br>\
+						- Disciplina Obrigatória</br><br>\
+						<b>Carga Horária</b></br><br>\
+						- Horas Teóricas: 32h</br>\
+						- Horas Práticas: 00h</br>\
+						- Carga horária total: 32h<br></br>\
+						<b>Pré-Requisitos</b></br><br>\
+						- Nenhum</br>\
+						Pré-Requisito para(disciplina(s) que só pode(m) ser cursada(s) após Computação e Sociedade)</br>\
+						- Nenhum</br><br>\
+						<b>Ementa(corresponde ao conteúdo que será abordado durante as aulas)</b></br><br>\
+						- História da computação.<br>\
+						- Estudo e análise de casos de aplicação de computadores na sociedade e para o meio ambiente.<br>\
+						- Subáreas da computação e áreas interdisciplinares.<br>\
+						- Importância e desafios da computação no Brasil e no mundo.<br>\
+						- Cursos de computação e aspectos profissionais: tipos de cursos, perfis profissionais, demanda do mercado, organizações e associações na área, regulamentação da profissão.<br>\
+						- Leis e normas relacionadas à Informática.<br>\
+						- Questões ambientais, raciais, de saúde e de inclusão digital relacionadas à Computação.<br>\
+						- Ética na Computação.<br>\
+						- Empresas de tecnologia da informação.<br>\
+						- Incubadoras de empresas.'
 		},
 		{
 			titulo: 'IntProgramacao',
-			conteudo: 'TESTETESTETESTE'	
+			conteudo: '<b>Características</b></br><br>\
+						- Núcleo Comum</br>\
+						- Disciplina ministrada pelo INF (Instituto de Informática)</br>\
+						- Disciplina Obrigatória</br><br>\
+						<b>Carga Horária</b></br><br>\
+						- Horas Teóricas: 48h</br>\
+						- Horas Práticas: 80h</br>\
+						- Carga horária total: 128h<br></br>\
+						<b>Pré-Requisitos</b></br><br>\
+						- Nenhum</br>\
+						Pré-Requisito para(disciplina(s) que só pode(m) ser cursada(s) após Computação e Sociedade)</br>\
+						Diretamente<br>\
+						- Programação Orientada a Objetos</br>\
+						- Linguagens e Paradigmas de Programação</br>\
+						- Algoritmos e Estruturas de Dados 1</br></br>\
+						Indiretamente<br>\
+						- Projeto de Software</br>\
+						- Algoritmos e Estruturas de Dados 2</br>\
+						- Análise e Projeto de Algoritmos</br>\
+						- Interação Humano-Computador</br>\
+						- Compiladores</br>\
+						- Sistemas Distribuídos</br></br>\
+						<b>Ementa(corresponde ao conteúdo que será abordado durante as aulas)</b></br><br>\
+						- Introdução a algoritmos.<br>\
+						- Conceitos básicos de programas: constantes; tipos de dados primitivos; variáveis; atribuição; entrada e saída de dados; expressões; estruturas de decisão; estruturas de repetição.<br>\
+						- Ponteiro.<br>\
+						- Estruturas de dados homogêneas e heterogêneas: vetores, matrizes, cadeias de caracteres, registros. Subprogramas: funções; passagens de parâmetros por valor e por referência, recursividade.<br>\
+						- Manipulação de arquivos: abertura, fechamento, leitura e gravação.<br>\
+						- Tipos de acesso a arquivos: sequencial e indexado.<br>\
+						- Tipos de arquivos (texto e binário).<br>\
+						- Transcrição de algoritmos para uma linguagem de programação.<br>\
+						- Domínio de uma linguagem de programação: sintaxe e semântica; interpretação e compilação de programas; ambiente de desenvolvimento de programas; estilo de codificação; documentação de código; técnicas de depuração e técnicas de profiling; desenvolvimento e uso de bibliotecas.'
 		},
 		{
 			titulo: 'Calculo',
-			conteudo: 'O Bacharelado em Engenharia de Software foi criado em 2009, sendo a UFG a universidade brasileira pioneira. O curso é focado na graduação de um profissional capaz de especificar software; projetar (design) software (inclui arquitetura de software); construir (programar) software com qualidade; realizar atividades de manutenção de software, além de trabalhar em equipe, comunicar e escrever bem. Tem a duração de 9 semestres, predominantesmente no turno noturno.'	
+			conteudo: '<b>Características</b></br><br>\
+						- Núcleo Comum</br>\
+						- Disciplina ministrada pelo IME (Instituto de Matemática e Estatística)</br>\
+						- Disciplina Obrigatória</br><br>\
+						<b>Carga Horária</b></br><br>\
+						- Horas Teóricas: 96h</br>\
+						- Horas Práticas: 00h</br>\
+						- Carga horária total: 96h<br></br>\
+						<b>Pré-Requisitos</b></br><br>\
+						- Nenhum</br>\
+						Pré-Requisito para(disciplina(s) que só pode(m) ser cursada(s) após Computação e Sociedade)</br>\
+						Diretamente<br>\
+						- Probabilidade e Estatística A</br></br>\
+						Indiretamente<br>\
+						- Interação Humano-Computador</br></br>\
+						<b>Ementa(corresponde ao conteúdo que será abordado durante as aulas)</b></br><br>\
+						- Números reais. Funções reais de uma variável real e suas inversas.<br>\
+						- Noções sobre cônicas.<br>\
+						- Limite e continuidade.<br>\
+						- Derivadas e aplicações.<br>\
+						- Polinômio de Taylor.<br>\
+						- Integrais. Técnicas de Integração. Integrais impróprias.<br>\
+						- Aplicações.'	
 		},
 		{
 			titulo: 'FundMatematica',
-			conteudo: 'TESTETESTETESTE'	
+			conteudo: '<b>Características</b></br><br>\
+						- Núcleo Comum</br>\
+						- Disciplina ministrada pelo INF (Instituto de Informática)</br>\
+						- Disciplina Obrigatória</br><br>\
+						<b>Carga Horária</b></br><br>\
+						- Horas Teóricas: 64h</br>\
+						- Horas Práticas: 00h</br>\
+						- Carga horária total: 64h<br></br>\
+						<b>Pré-Requisitos</b></br><br>\
+						- Nenhum</br>\
+						Pré-Requisito para(disciplina(s) que só pode(m) ser cursada(s) após Computação e Sociedade)</br>\
+						Diretamente<br>\
+						- Lógica Matemática</br>\
+						- Arquitetura de Computadores</br>\
+						- Análise e Projeto de Algoritmos</br>\
+						- Linguagens Formais e Autômatos</br></br>\
+						Indiretamente<br>\
+						- Banco de Dados</br>\
+						- Sistemas Operacionais</br>\
+						- Compiladores</br></br>\
+						<b>Ementa(corresponde ao conteúdo que será abordado durante as aulas)</b></br><br>\
+						- Noções de lógica.<br>\
+						- Introdução a demonstrações.<br>\
+						- Indução matemática, Recursividade e Relações de Recorrência, Conjuntos e Combinatória.<br>\
+						- Séries e sequências.<br>\
+						- Relações e Funções.<br>\
+						- Representações numéricas e Mudança de base numérica.'	
 		},
 	];
 
