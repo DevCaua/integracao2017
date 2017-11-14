7d9d85daf00285d10e16413c2035ccf8deb9f330
Cauã M. Pessoa <caua539@live.com>
2017-11-14 23:54:42 +0000

    1) Remoção da persistência de tamanho de fontes entre páginas;

--- scripts/pages-controller.js
+++ scripts/pages-controller.js
@@ -16,22 +16,6 @@ angular.module('ocurso').controller('PagesController', function($scope, $window,
   $scope.arrayAcessibilidadeTotal = [];
   $scope.arrayRecebido = [];
 
-  //variaveis para acessibilidade de fonte
-  $scope.tamanho = 13;
-  $scope.tamanhoNavItem;
-  $scope.tamanhoLogoSmall;
-  $scope.tamanhoLogoLarge;
-  $scope.tamanhoNavLateral;
-  $scope.tamanhoNomeBotao;
-  $scope.tamanhoDescricaoItem;
-
-  $scope.tamanhoFonte = $window.fonte;
-  $scope.tamanhoFonteNavItem = $window.fonteNavItem;
-  $scope.tamanhoFonteLogoSmall = $window.fonteLogoSmall;
-  $scope.tamanhoFonteLogoLarge = $window.fonteLogoLarge;
-  $scope.tamanhoFonteNavLateral = $window.fonteNavLateral;
-  $scope.tamanhoFonteNomeBotao = $window.fonteNomeBotao;
-  $scope.tamanhoFonteDescricaoItem = $window.fonteDescricaoItem;
 
   //função responsável por ler os dados do localStorage ao carregar o controller
   (function () {
@@ -61,22 +45,8 @@ angular.module('ocurso').controller('PagesController', function($scope, $window,
   $scope.acessibilidadeByellow = $scope.arrayConvertidoParaBoolean[1];
   $scope.acessibilidadeYellow = $scope.arrayConvertidoParaBoolean[2];
   $scope.acessibilidadeNula = $scope.arrayConvertidoParaBoolean[3];
-  $scope.tamanhoFonte = $scope.arrayConvertidoParaBoolean[4];
-  $scope.tamanhoFonteNavItem = $scope.arrayConvertidoParaBoolean[5];
-  $scope.tamanhoFonteLogoSmall = $scope.arrayConvertidoParaBoolean[6];
-  $scope.tamanhoFonteLogoLarge = $scope.arrayConvertidoParaBoolean[7];
-  $scope.tamanhoFonteNavLateral = $scope.arrayConvertidoParaBoolean[8];
-  $scope.tamanhoFonteNomeBotao = $scope.arrayConvertidoParaBoolean[9];
-  $scope.tamanhoFonteDescricaoItem = $scope.arrayConvertidoParaBoolean[10];
 
-  // define os tamanhos para view
-  $scope.tamanho = $scope.tamanhoFonte;
-  $scope.tamanhoNavItem = $scope.tamanhoFonteNavItem;
-  $scope.tamanhoLogoSmall = $scope.tamanhoFonteLogoSmall;
-  $scope.tamanhoLogoLarge = $scope.tamanhoFonteLogoLarge;
-  $scope.tamanhoNavLateral = $scope.tamanhoFonteNavLateral;
-  $scope.tamanhoNomeBotao = $scope.tamanhoFonteNomeBotao;
-  $scope.tamanhoDescricaoItem = $scope.tamanhoFonteDescricaoItem;
+
 
   // determina as classes da acessibilidade selecionada
   $scope.buscaAcessibilidade = function(tipoAcessibilidade){
@@ -94,88 +64,6 @@ angular.module('ocurso').controller('PagesController', function($scope, $window,
       localStorage.setData(arrayAcessibilidade);
   }
 
-  // atribui o tamanho da fonte de acordo com a acessibilidade escolhida
-  $scope.atribuiAcessibilidadeFonte = function(tipoFonte){
-    $scope.inicializaFonte();
-    if(acessibilidadeFonteMaior == tipoFonte){
-      $scope.tamanhoFonte = $window.fonte+3;
-      $scope.tamanhoFonteNavItem = $window.fonteNavItem+3;
-      $scope.tamanhoFonteLogoSmall = $window.fonteLogoSmall+3;
-      $scope.tamanhoFonteLogoLarge = $window.fonteLogoLarge+3;
-      $scope.tamanhoFonteNavLateral = $window.fonteNavLateral+3;
-      $scope.tamanhoFonteNomeBotao = $window.fonteNomeBotao+3;
-      $scope.tamanhoFonteDescricaoItem = $window.fonteDescricaoItem+3;
-    }else{
-      $scope.tamanhoFonte = $window.fonte-3;
-      $scope.tamanhoFonteNavItem = $window.fonteNavItem-3;
-      $scope.tamanhoFonteLogoSmall = $window.fonteLogoSmall-3;
-      $scope.tamanhoFonteLogoLarge = $window.fonteLogoLarge-3;
-      $scope.tamanhoFonteNavLateral = $window.fonteNavLateral-3;
-      $scope.tamanhoFonteNomeBotao = $window.fonteNomeBotao-3;
-      $scope.tamanhoFonteDescricaoItem = $window.fonteDescricaoItem-3;
-    }
-    $scope.tamanho = $scope.tamanhoFonte;
-    $scope.colocaValoresNaLista();
-    $scope.atribuiValorFonte();
-  }
-
-  // atribui valor para localStorage
-  $scope.atribuiValorFonte = function(){
-    arrayAcessibilidade[4] = $scope.tamanhoFonte;
-    arrayAcessibilidade[5] = $scope.tamanhoFonteNavItem;
-    arrayAcessibilidade[6] = $scope.tamanhoFonteLogoSmall;
-    arrayAcessibilidade[7] = $scope.tamanhoFonteLogoLarge;
-    arrayAcessibilidade[8] = $scope.tamanhoFonteNavLateral;
-    arrayAcessibilidade[9] = $scope.tamanhoFonteNomeBotao;
-    arrayAcessibilidade[10] = $scope.tamanhoFonteDescricaoItem;
-    arrayAcessibilidade = $scope.trocaValorNuloFonte(arrayAcessibilidade);
-    console.log(arrayAcessibilidade);
-    localStorage.setData(arrayAcessibilidade);
-  }
-
-  //coloca valores na lista
-  $scope.colocaValoresNaLista = function(){
-    $scope.arrayAcessibilidadeFonte.push($scope.tamanhoFonte);
-    $scope.arrayAcessibilidadeFonte.push($scope.tamanhoFonteNavItem);
-    $scope.arrayAcessibilidadeFonte.push($scope.tamanhoFonteLogoSmall);
-    $scope.arrayAcessibilidadeFonte.push($scope.tamanhoFonteLogoLarge);
-    $scope.arrayAcessibilidadeFonte.push($scope.tamanhoFonteNavLateral);
-    $scope.arrayAcessibilidadeFonte.push($scope.tamanhoFonteNomeBotao);
-    $scope.arrayAcessibilidadeFonte.push($scope.tamanhoFonteDescricaoItem);
-  }
-
-  $scope.trocaValorNuloFonte = function(arrayAcessibilidade){
-    var nan = "NaN"
-    for(var i = 4; i < 11; i++){
-      console.log(arrayAcessibilidade[i].toString() == nan);
-      if(arrayAcessibilidade[i] == 'undefined' || arrayAcessibilidade[i].toString() == nan){
-        console.log(i);
-        if(i == 4){
-          arrayAcessibilidade[i] = $scope.tamanhoFonte;
-        }else if(i == 5){
-          arrayAcessibilidade[i] = $scope.tamanhoFonteNavItem;
-        }else if(i == 6){
-          arrayAcessibilidade[i] = $scope.tamanhoFonteLogoSmall;
-        }else if(i == 7){
-          arrayAcessibilidade[i] = $scope.tamanhoFonteLogoLarge;
-        }else if(i == 8){
-          arrayAcessibilidade[i] = $scope.tamanhoFonteNavLateral;
-          console.log($scope.tamanhoFonteNavLateral);
-        }else if(i == 9){
-          arrayAcessibilidade[i] = $scope.tamanhoFonteNomeBotao;
-        }else if(i == 10){
-          arrayAcessibilidade[i] = $scope.tamanhoFonteDescricaoItem;
-        }
-      }
-    }
-    return arrayAcessibilidade;
-  }
-
- //inicializa as fontes
-  $scope.inicializaFonte = function(){
-    $scope.arrayAcessibilidadeFonte = [];
-    localStorage.clearData();
-  }
 
   // Inicializa todas variáveis
   $scope.inicializa = function(){
